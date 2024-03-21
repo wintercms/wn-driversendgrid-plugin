5a33df2baa9fc42b5659982d94d9dc741cc4085c
Marc Jauvin <marc.jauvin@gmail.com>
2024-03-21 10:59:30 -0400

    set default value from config

--- Plugin.php
+++ Plugin.php
@@ -60,6 +60,7 @@ class Plugin extends PluginBase
             $model->bindEvent('model.beforeValidate', function () use ($model) {
                 $model->rules['sendgrid_api_key'] = 'required_if:send_mode,' . self::MODE_SENDGRID;
             });
+            $model->sendgrid_api_key = config('services.sendgrid.api_key');
         });
 
         Event::listen('backend.form.extendFields', function ($widget) {
